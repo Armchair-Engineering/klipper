0dc5ed4407e77286041b2b7e0296b397966f14c6
Brandon Nance <bwnance@gmail.com>
2022-10-20 22:35:47 -0400

    garhs

--- klippy/kinematics/polarxz.py
+++ klippy/kinematics/polarxz.py
@@ -316,8 +316,8 @@ class PolarXZKinematics:
                 if len(intersections) == 0:
                     logging.error("no intersections found!")
                 move.end_pos = (intersections[-1][0], intersections[-1][1], move.end_pos[2], move.end_pos[3])
-                logging.info("intersections: %s" % intersections)
-                logging.info("new end pos: %s" % move.end_pos)
+                logging.info("intersections: %s", intersections)
+                logging.info("new end pos: %s", move.end_pos)
 
         if move.axes_d[2]:
             if end_pos[2] < self.limit_z[0] or end_pos[2] > self.limit_z[1]:
