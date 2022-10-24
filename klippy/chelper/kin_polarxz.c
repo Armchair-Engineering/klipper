90e46497626fdf708751b88af5eb00526ac9d3d0
Brandon Nance <bwnance@gmail.com>
2022-10-24 19:36:32 -0400

    welp

--- klippy/chelper/kin_polarxz.c
+++ klippy/chelper/kin_polarxz.c
@@ -1,12 +1,13 @@
-#include <math.h>      // sqrt
-#include <stdlib.h>    // malloc
-#include <string.h>    // memset
-#include "compiler.h"  // __visible
+#include <math.h> // sqrt
+#include <stdlib.h> // malloc
+#include <string.h> // memset
+#include "compiler.h" // __visible
 #include "itersolve.h" // struct stepper_kinematics
-#include "trapq.h"     // move_get_coord
+#include "trapq.h" // move_get_coord
 
 static double
-polarxz_stepper_angle_calc_position(struct stepper_kinematics *sk, struct move *m, double move_time)
+polarxz_stepper_angle_calc_position(struct stepper_kinematics *sk
+                                    , struct move *m, double move_time)
 {
     struct coord c = move_get_coord(m, move_time);
     // XXX - handle x==y==0
@@ -28,17 +29,16 @@ polarxz_stepper_angle_post_fixup(struct stepper_kinematics *sk)
         sk->commanded_pos -= 2 * M_PI;
 }
 
-struct stepper_kinematics *__visible
+struct stepper_kinematics * __visible
 polarxz_stepper_alloc(char type)
 {
     struct stepper_kinematics *sk = malloc(sizeof(*sk));
     memset(sk, 0, sizeof(*sk));
-    if (type == 'a')
-    {
+    if (type == 'a') {
         sk->calc_position_cb = polarxz_stepper_angle_calc_position;
         sk->post_cb = polarxz_stepper_angle_post_fixup;
         sk->active_flags = AF_X | AF_Y;
     }
-
+    
     return sk;
 }
