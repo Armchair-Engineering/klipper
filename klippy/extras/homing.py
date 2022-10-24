52c162d2c2bfe74c5faeb9064f108d094b1d1fca
Brandon Nance <bwnance@gmail.com>
2022-10-23 23:25:19 -0400

    fix

--- klippy/extras/homing.py
+++ klippy/extras/homing.py
@@ -156,6 +156,7 @@ class HomingMove:
             return None
         for sp in self.stepper_positions:
             logging.info("sp endstop name: %s", sp.endstop_name)
+            logging.info("sp stepper name: %s", sp.stepper_name)
             logging.info('sp start_pos: %s', sp.start_pos)
             logging.info('sp halt_pos: %s', sp.halt_pos)
             if sp.start_pos == sp.trig_pos:
