321cbfa6abc8429b4dea8c48145e9c02df6b51ce
Brandon Nance <bwnance@gmail.com>
2023-02-08 03:30:15 -0500

    too late for this shit jesus

--- klippy/extras/force_move.py
+++ klippy/extras/force_move.py
@@ -127,10 +127,11 @@ def calc_move_time_polar(angle, speed, accel):
         if num_segments == 1:
             decel_t = accel_t
             cruise_t -= decel_t
-        if state != "decelerating":
+        elif state != "decelerating":
             decel_t = 0
         move = (cartesian_end[0], cartesian_end[1], x_ratio, y_ratio, round(accel_t,10), round(cruise_t,10), round(decel_t, 10), speed)
         moves.append(move)
+        print(num_segments)
         cartesian_start = cartesian_end
    
     return moves
